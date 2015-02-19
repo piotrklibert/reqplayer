e7881d5541486399054aeb7495d876fb741ebab5
Piotr Klibert <piotr.klibert@10clouds.com>
2015-02-19 01:24:24 +0100

    Removed unnecessary prints

--- src/reqviewer_testsuites.erl
+++ src/reqviewer_testsuites.erl
@@ -16,5 +16,5 @@ dispatch_action(addtest, Req) ->
     ReqQS = cowboy_req:parse_qs(Req),
     SuiteId = proplists:get_value(<<"suite_id">>, ReqQS),
     TestData = proplists:get_value(<<"test_data">>, ReqQS),
-    io:format("~p~p", [SuiteId, TestData]),
+    %% io:format("~p~p", [SuiteId, TestData]),
     data_store:add_test(SuiteId, TestData).
