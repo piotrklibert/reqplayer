e2204a53c105e54b3a2ada1c0af9c8858284c674
Piotr Klibert <piotr.klibert@10clouds.com>
2015-03-19 18:36:11 +0000

    Date added to logs

--- src/t.erl
+++ src/t.erl
@@ -2,6 +2,25 @@
 
 -compile(export_all).
 
+day_changed() ->
+    spawn_link(?MODULE, day_changed, [calendar:local_time()]).
+
+day_changed(PrevDate) ->
+    CurrentDate = {{_,_,Today}, _} = calendar:local_time(),
+    case PrevDate of
+        {{_,_,Today}, {_,_,_}} ->
+            %% nothing to do here, we just wait a bit and check again later
+            receive after 5000 -> day_changed(PrevDate) end;
+
+        _ ->
+            %% the day have changed!
+            io:format("New day has arrived! It's ~p now", [CurrentDate]),
+            day_changed(CurrentDate)
+    end.
+
+
+
+
 fmt(Term) ->
     io_lib:format("~p", [Term]).
 
